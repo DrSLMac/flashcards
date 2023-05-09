67fe0b5dceb35a497e05c9543c56ea9a31670e52
Shauna MacFarlane-Okongo <shauna.macfarlane579@gmail.com>
2023-05-09 18:05:07 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+// This is where your project starts.
+
+console.log('Your project is running...'); 
---
+++ src/game.js
@@ -0,0 +1,14 @@
+const data = require('./data');
+const prototypeQuestions = data.prototypeData;
+const util = require('./util');
+
+function printMessage(deck) {
+  console.log(`Welcome to FlashCards! You are playing with ${countCards(deck)} cards.
+  -----------------------------------------------------------------------`);
+}
+
+function printQuestion(round) {
+  util.main(round);
+}
+
+module.exports = { printMessage, printQuestion };
